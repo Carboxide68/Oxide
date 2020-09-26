e031d7014fabc742d1705d44e179de81ef56e3fa
Carboxide68 <co.dincrod@gmail.com>
2020-09-27 00:19:46 +0200

    Added stb_image and made a camera class, none tested yet

---
+++ Oxide/src/Oxide/Renderer/Camera.cpp
@@ -0,0 +1,129 @@
+#include "Oxide/Renderer/Camera.h"
+#include "glm/trigonometric.hpp"
+
+namespace Oxide {
+
+    PerspectiveCamera::PerspectiveCamera() {
+
+        m_ViewMatrix = glm::mat4(1);
+
+    }
+
+    const float& PerspectiveCamera::GetFOV() const {
+        return glm::degrees(m_FOV);
+    }
+
+    const float& PerspectiveCamera::GetFOVRadians() const {
+        return m_FOV;
+    }
+
+    void PerspectiveCamera::SetFOV(const float& degrees) {
+        SetFOVRadians(glm::radians(degrees));
+    }
+
+    void PerspectiveCamera::SetFOVRadians(const float& radians) {
+        m_FOV = radians;
+        UpdatePerspectiveMatrix();
+
+    }
+
+    void PerspectiveCamera::SetNear(const float& near) {
+        m_Near = near;
+        UpdatePerspectiveMatrix();
+    }
+
+    void PerspectiveCamera::SetFar(const float& far) {
+        m_Far = far;
+        UpdatePerspectiveMatrix();
+    }
+
+    const float& PerspectiveCamera::GetNear() const {
+        return m_Near;
+    }
+
+    const float& PerspectiveCamera::GetFar() const {
+        return m_Far;
+    }
+
+    const glm::mat4& PerspectiveCamera::GetViewMatrix() const {
+        return m_ViewMatrix;
+    }
+
+    const glm::mat4& PerspectiveCamera::GetPerspectiveMatrix() const {
+        return m_PerspectiveMatrix;
+    }
+    
+    const glm::vec3& PerspectiveCamera::GetPosition() const {
+
+        glm::vec3 position;
+        position.x = m_ViewMatrix[3][0];
+        position.y = m_ViewMatrix[3][1];
+        position.z = m_ViewMatrix[3][2];
+
+        return position;
+    }
+
+    void PerspectiveCamera::SetPosition(const glm::vec3& position) {
+        m_ViewMatrix[3][0] = position.x;
+        m_ViewMatrix[3][1] = position.y;
+        m_ViewMatrix[3][2] = position.z;
+    }
+
+    void PerspectiveCamera::Move(const glm::vec3& position) {
+        m_ViewMatrix[3][0] += position.x;
+        m_ViewMatrix[3][1] += position.y;
+        m_ViewMatrix[3][2] += position.z;
+    }
+                                                                //TODO: Make sure that it doesn't become bad if position is too close to x = 1
+    void PerspectiveCamera::LookAt(const glm::vec3& position) { //FIXME: Test if this actually works, I have no idea
+        glm::vec3 UP = glm::vec3(0, 1, 0);
+
+        glm::vec3 z = glm::normalize(position);
+        glm::vec3 x = glm::cross(z, UP); //Will always be normalized since we have 2 vectors with length 1
+        glm::vec3 y = glm::cross(x, z);
+        z *= -1;
+
+        glm::vec3 p = -GetPosition(); //Position, need it to be n.gative in the matrix for things to be translated correctly
+
+        m_ViewMatrix = glm::mat4(
+            x.x, x.y, x.z, 0,
+            y.x, y.y, y.z, 0,
+            z.x, z.y, z.z, 0,
+            p.x, p.y, p.z, 1
+        );
+    }
+    
+    const glm::vec3& PerspectiveCamera::GetLookAt() const {
+        glm::vec3 position;
+
+        position.x = m_ViewMatrix[2][0];
+        position.y = m_ViewMatrix[2][1];
+        position.z = m_ViewMatrix[2][2];
+
+        return -position;
+    }
+
+    static Ref<PerspectiveCamera> Create() {
+
+        return CreateRef<PerspectiveCamera>();
+
+    }
+
+    void PerspectiveCamera::UpdatePerspectiveMatrix() {
+
+        float S = 1/tan(m_FOV/2);
+
+        float tmp1 = m_Far/(m_Far - m_Near);
+        float tmp2 = (m_Far * m_Near)/(m_Far - m_Near);
+
+        m_PerspectiveMatrix = glm::mat4(
+            S,  0,  0   ,   0   ,
+            0,  S,  0   ,   0   ,
+            0,  0,  tmp1,   tmp2,
+            0,  0, -1   ,   0   
+
+        );
+
+    }
+
+}
--- Oxide/src/Oxide/Renderer/Camera.h
+++ Oxide/src/Oxide/Renderer/Camera.h
@@ -1,33 +1,58 @@
 #pragma once
 
 #include "Oxide/Core/Base.h"
+#include "glm/mat4x4.hpp"
+#include "glm/vec3.hpp"
 
 namespace Oxide {
 
     //TODO: Fix math library
 
-    class Camera {
-
-        virtual ~Camera() = default;
-
-        //virtual const glm::mat4& GetViewMatrix();
-
-    };
-
-    class PerspectiveCamera : Camera {
+    class PerspectiveCamera {
+    public:
 
         PerspectiveCamera();
+        
+        const glm::mat4& GetViewMatrix() const;
+        const glm::mat4& GetPerspectiveMatrix() const;
 
-        //virtual const glm::mat4& GetViewMatrix();
+        const float& GetFOV() const;
+        const float& GetFOVRadians() const;
+        void SetFOV(const float& degrees);
+        void SetFOVRadians(const float& radians);
+
+        void SetNear(const float& near);
+        void SetFar(const float& far);
+        const float& GetNear() const;
+        const float& GetFar() const;
+
+        const glm::vec3& GetPosition() const;
+        void SetPosition(const glm::vec3& position);
+        void Move(const glm::vec3& position);
+
+        void LookAt(const glm::vec3& position); //Looks at object without rotating around x-axis
+        const glm::vec3& GetLookAt() const; //Returns a vector with a length of 1 in the direction which it's looking at
+
+        static Ref<PerspectiveCamera> Create();
+
+    private:
+
+        void UpdatePerspectiveMatrix();
+
+        glm::mat4 m_ViewMatrix;
+        glm::mat4 m_PerspectiveMatrix;
+
+        float m_FOV;
+        float m_Near;
+        float m_Far;
 
     };
 
-    class OrthographicCamera : Camera { 
+    class OrthographicCamera {  //TODO: Implement Orthographic camera
 
         OrthographicCamera();
 
-        //virtual const glm::mat4& GetViewMatrix();
-
     };
 
+    typedef PerspectiveCamera Camera;
 }
